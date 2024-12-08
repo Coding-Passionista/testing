da7f46f9d1417488cb39f7fa47d027639e800115
Euphoric-Coder <147186298+Euphoric-Coder@users.noreply.github.com>
2024-12-08 22:10:03 +0530

    added a new line

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div>
         This is just a test for the following taks...... 
-        
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At, facere. Distinctio reiciendis commodi voluptate illum itaque, possimus tempore voluptatum dicta iure consequatur natus!
     </div>
   )
 }
